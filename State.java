e90600373baa187668da517611c8128db9cb8b68
ROHIT <rohitgangurde@u.boisestate.edu>
2019-09-21 13:18:22 -0600

    Updated State.java

--- State.java
+++ State.java
@@ -1,5 +1,19 @@
-/* List <Bag> totalBags : a list of type Bag, containing maximum number of bag objects
-   List <Item> itemsNotAdded : a list of type Item, containing the items not added to the bags
-   boolean isComplete() : if the itemsNotAdded list is empty, returns true; false otherwise
-   List <State> nextPossibleStates : a list of type state, contains next possible states
-   */
+/
+
+public class State{
+    private List<Bag> totalBags;
+    private List<Item> itemsNotAdded;
+
+    public State(List<Bag> bags, List<Item> items){
+        totalBags= bags;
+        itemsNotAdded= items;
+    }
+
+    List<State> nextPossibleStates(){
+        return  null;
+    }
+
+    boolean isComplete(){
+        return false;
+    }
+}
